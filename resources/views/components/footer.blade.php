4aeb1a0588ded8142cd67e3fe444d5e9a4eb9161
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-11 01:27:23 +0600

    update footer content

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,5 @@
-<footer>
-    footer section
+<footer class="bg-white/30 py-4 backdrop-blur-xs">
+    <p class="text-center text-sm text-gray-500">
+        Copyright {{  date('Y') }} All rights reserved.
+    </p>
 </footer>
