3e100177c5950c09ee259ce9ae4790de2c41d9cc
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-11 21:53:48 +0600

    add featured recipe, categories, recipes grid and newsletter component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,13 @@
 <x-layout>
 
     <x-home.featured-recipe />
+
+    <x-home.featured-recipes />
+
+    <x-home.featured-categories />
+
+    <x-home.chef-blog />
+
+    <x-home.newslatter />
     
 </x-layout>
