15c88ad5610517ce299835c9e1c32693bc97b176
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-11 21:38:37 +0600

    update footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,23 @@
-<footer class="bg-white/30 py-4 backdrop-blur-xs">
-    <p class="text-center text-sm text-gray-500">
-        Copyright {{  date('Y') }} All rights reserved.
-    </p>
+<footer class="bg-white/30 py-4 backdrop-blur-xs max-w-7xl mx-auto flex flex-col">
+    <a class="logo" href="/">Foodieland<span class="text-orange-600">.</span></a>
+
+    <div class="flex gap-14 justify-end">
+        <a href="/recipes" class="navlink">Recipes</a>
+        <a href="/blog" class="navlink">Blog</a>
+        <a href="/contact" class="navlink">Contact</a>
+        <a href="/about" class="navlink">About Us</a>
+    </div>
+
+    <p class="text-black/60 mb-10">Lorem ipsum dolor sit amet, consectetuipisicing elit, </p>
+    
+    <div class="w-full h-px bg-black/10"></div>
+
+    <div class="flex justify-between items-center my-10">
+        <p class="text-black/60 text-lg font-medium flex-1 text-center">&copy; <span class="text-orange-600">Foodieland</span> all right reserved.</p>
+        <div class="flex items-center gap-10">
+            <box-icon type='logo' name='facebook'></box-icon>
+            <box-icon type='logo' name='twitter'></box-icon>
+            <box-icon name='instagram' type='logo'></box-icon>
+        </div>
+    </div>
 </footer>
