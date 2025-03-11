a7dbf592ac460e61ee6b3ec4a83fe66c0fb2f735
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-12 00:49:19 +0600

    create recipe related migrations

---
+++ database/migrations/2025_03_11_173457_create_recipes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->json('ingredients'); // Store ingredients as JSON
+            $table->json('instructions'); // Store instructions as JSON
+            $table->integer('prepTimeMinutes');
+            $table->integer('cookTimeMinutes');
+            $table->integer('servings');
+            $table->string('difficulty');
+            $table->string('cuisine');
+            $table->integer('caloriesPerServing');
+            $table->string('image');
+            $table->timestamps();
+
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
---
+++ database/migrations/2025_03_11_182130_create_reviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Recipe;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Recipe::class)->constrained()->cascadeOnDelete(); // Foreign key to recipes
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete(); // Foreign key to users
+            $table->text('comment'); // Review comment
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
