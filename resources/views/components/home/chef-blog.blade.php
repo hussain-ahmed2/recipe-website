6e62ab41115dc7a97b9f3322f35c614a38d01f8b
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-11 21:52:34 +0600

    add chef article

---
+++ resources/views/components/home/chef-blog.blade.php
@@ -0,0 +1,15 @@
+<div class="max-w-7xl mx-auto mt-20 h-[37.5rem] flex gap-11 rounded-3xl overflow-hidden">
+    <div class="flex flex-col gap-8 justify-center">
+        <h2 class="font-semibold text-5xl">Everyone can be a <br>
+            chef in their own kitchen</h2>
+        <p class="text-black/60">Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqut enim ad minim </p>
+
+        <button class="mt-10 bg-black rounded-2xl px-9 py-4 flex items-center gap-3 text-white w-fit">
+            Learn More
+        </button>
+    </div>
+    <div class="h-[37.5rem] w-full">
+        <img class="w-full h-full rounded-3xl" src="{{ Vite::asset('resources/images/chef.png') }}" alt="chef image">
+    </div>
+</div>
