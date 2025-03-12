1f285fb428adf4b16cfba7f0e10bc17245b57cd6
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-13 02:34:15 +0600

    Update CSS and JavaScript assets with new styles and functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+const menu = document.getElementById("menu");
+const menuButton = document.getElementById("menu-button");
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("max-md:translate-x-full");
+});
+
+window.addEventListener('click', (e) => {
+    if (!menu.contains(e.target) && !menuButton.contains(e.target)) {
+        menu.classList.add('max-md:translate-x-full');
+    }
+});
