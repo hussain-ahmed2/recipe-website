1a6552e0974a6502fd09ca2c018e93001eda6b3e
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-15 02:48:08 +0600

    Update Recipe model relationships and attributes

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Recipe extends Model
 {
@@ -21,5 +23,10 @@ class Recipe extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function favouritedBy(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'favourites')->withTimestamps();
     }
 }
