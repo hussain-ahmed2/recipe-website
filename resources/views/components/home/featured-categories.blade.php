ffd7baf0d657df6b02723f4d64420015b912f780
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-14 10:27:31 +0600

    update the category card make it a link

--- resources/views/components/home/featured-categories.blade.php
+++ resources/views/components/home/featured-categories.blade.php
@@ -7,11 +7,12 @@
     </div>
     <div class="flex justify-center flex-wrap gap-5 md:gap-10">
         @foreach ($categories as $category)
-            <div
-                class="max-w-42 flex flex-col items-center justify-between gap-10 p-6 bg-gradient-to-b from-white to-orange-50 rounded-3xl md:rounded-4xl">
+            <a 
+                href="/categories/{{ $category->id }}"
+                class="max-w-42 flex flex-col items-center justify-between gap-10 p-6 bg-gradient-to-b from-white to-orange-50 hover:from-orange-50 hover:to-orange-100 tc rounded-3xl md:rounded-4xl">
                 <div class="text-6xl md:text-8xl drop-shadow-2xl">{{ $category->icon }}</div>
                 <h4 class="font-semibold text-lg">{{ $category->name }}</h4>
-            </div>
+            </a>
         @endforeach
     </div>
     <div class="w-full md:hidden mt-5 md:mt-10">
