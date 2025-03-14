14e8695b1382adc4528881bc9224eefd36a117ba
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-15 02:48:14 +0600

    Add recipe relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -52,13 +53,8 @@ class User extends Authenticatable
         return $this->hasMany(Recipe::class);
     }
 
-    public function reviews(): HasMany
+    public function favourites(): BelongsToMany
     {
-        return $this->hasMany(Review::class);
-    }
-
-    public function ratings(): HasMany
-    {
-        return $this->hasMany(Rating::class);
+        return $this->belongsToMany(Recipe::class, 'favourites')->withTimestamps();
     }
 }
