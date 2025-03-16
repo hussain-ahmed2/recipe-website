a3f5b78b7abcb7eb11f841583e1de3e3494e9e50
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-17 01:06:54 +0600

    refactor: remove user recipe management methods from RecipeController

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -46,38 +46,6 @@ class RecipeController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('recipes.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => ['required'],
-            'ingredients' => ['required'],
-            'instructions' => ['required'],
-            'prepTimeMinutes' => ['required'],
-            'cookTimeMinutes' => ['required'],
-            'servings' => ['required'],
-            'difficulty' => ['required'],
-            'cuisine' => ['required'],
-            'caloriesPerServing' => ['required'],
-            'tags' => ['required'],
-            'image' => ['required'],
-            'mealType' => ['required']
-        ]);
-        $recipe = Recipe::create($request->all());
-
-        return redirect('/recipes/' . $recipe->id);
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(Recipe $recipe)
@@ -85,35 +53,5 @@ class RecipeController extends Controller
         return view('recipes.show', [
             'recipe' => $recipe
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Recipe $recipe)
-    {
-        return view('recipes.edit', [
-            'recipe' => $recipe
-        ]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Recipe $recipe)
-    {
-        $recipe->update($request->all());
-
-        return redirect('/recipes/' . $recipe->id);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Recipe $recipe)
-    {
-        $recipe->delete();
-
-        return redirect('/');
     }
 }
