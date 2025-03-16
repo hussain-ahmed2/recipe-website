8c1a28407ed82565c7c7bdec22075df84899bfeb
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-16 18:41:27 +0600

    add link to the category card

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -6,7 +6,7 @@
 
         <div class="flex justify-center flex-wrap gap-5 md:gap-10">
             @foreach ($categories as $category)
-                <a href="/categories/{{ $category->id }}"
+                <a href="/recipes?category={{ $category->id }}"
                     class="max-w-42 flex flex-col items-center justify-between gap-10 p-6 bg-gradient-to-b from-white to-orange-50 hover:from-orange-50 hover:to-orange-100 tc rounded-3xl md:rounded-4xl">
                     <div class="text-6xl md:text-8xl drop-shadow-2xl">{{ $category->icon }}</div>
                     <h4 class="font-semibold text-lg">{{ $category->name }}</h4>
