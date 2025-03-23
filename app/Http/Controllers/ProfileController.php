134c8c978452e5eee07f42808176fb7f811d6176
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-23 15:35:39 +0600

    update edit and update function

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,15 +17,30 @@ class ProfileController extends Controller
 
     public function edit()
     {
-        return view('profile.edit');
+        $user = Auth::user();
+        return view('profile.edit', compact('user'));
     }
 
     public function update(Request $request)
     {
+        $user = Auth::user();
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255'],
         ]);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        if ($request->filled('password')) {
+            $request->validate([
+                'password' => ['required', 'max:6', 'confirmed']
+            ]);
+
+            $user->password = $request->password;
+        }
+
+        $user->save();
 
         return redirect()->route('profile.edit')->with('success', 'Profile updated successfully.');
     }
