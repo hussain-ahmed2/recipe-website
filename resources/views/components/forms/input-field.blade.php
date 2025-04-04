3629c59f7a35bfef732818d4c2f7f43a2000a79a
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-04-04 16:03:00 +0600

    update input for supporting type range and number

--- resources/views/components/forms/input-field.blade.php
+++ resources/views/components/forms/input-field.blade.php
@@ -1,4 +1,15 @@
-@props(['name', 'label', 'type' => 'text', 'placeholder' => '', 'value' => '', 'required' => true, 'class' => ''])
+@props([
+    'name',
+    'label',
+    'type' => 'text',
+    'placeholder' => '',
+    'value' => '',
+    'required' => true,
+    'class' => '',
+    'min' => null,
+    'max' => null,
+    'step' => null
+])
 
 @php
     $defaults = [
@@ -7,9 +18,23 @@
         'type' => $type,
         'placeholder' => $placeholder,
         'value' => $value ? $value : old($name),
-        'class' => 'border border-black/10 outline-none py-3 px-4 rounded-lg w-full focus:ring-4 ring-black/10 transition duration-300 ' . $class,
-        'required' => $required
-    ]
+        'class' =>
+            'border border-black/10 outline-none py-3 px-4 rounded-lg w-full focus:ring-4 ring-black/10 transition duration-300 ' .
+            $class,
+        'required' => $required,
+    ];
+
+    if ($min) {
+        $defaults['min'] = $min;
+    }
+
+    if ($max) {
+        $defaults['max'] = $max;
+    }
+
+    if ($step) {
+        $defaults['step'] = $step;
+    }
 @endphp
 
 <div class="flex flex-col gap-1">
